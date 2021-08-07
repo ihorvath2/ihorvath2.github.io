52dd535ad0cf8ad5c042306f91cd0f5f1d4078e2
ihorvath2 <imre.horvath2@gmail.com>
2021-08-07 21:14:59 +0200

    Csere AppGallery-ra

---
+++ imagedata.js
@@ -0,0 +1,77 @@
+// Gallery app - Green Fox MyFirstApp Coding Camp
+// Képek adatai
+let kep0 = {
+   file: 'milan-boscoverticale.jpg',
+   cim: 'Milánó - Bosco Verticale',
+   leiras: 'A Bosco Verticale (Függőleges erdő) Milánóban két lakótorony elnevezése. Magasságuk 110, illetve 76 méter, különlegességüket az adja, hogy 8900 m2 összalapterületű erkélyeire és teraszaira mintegy 730 fát telepítettek',
+ };
+ let kep1 = {
+   file: 'tokyo-cocoontower.jpg',
+   cim: 'Tokió - Cocoon Tower',
+   leiras: 'A Mode Gakuen Cocoon Torony Tokióban a Sindzsuku kerületben található torony egy 204 m magas, 50 emeletes oktatási intézmény, Tokió Shinjuku kerületében.',
+ };
+ let kep2 = {
+   file: 'london-thegherkin.jpg',
+   cim: 'London - The Gherkin',
+   leiras: 'A 30 St Mary Axe (becenevén The Gherkin azaz Az Uborka), egy felhőkarcoló a londoni City-ben. 180 m magas, 2004-ben nyitották meg. Norman Foster és Ken Shuttleworth építészek tervezték.',
+ };
+ let kep3 = {
+   file: 'antwerpen-haven.jpg',
+   cim: 'Antwerpen - Havenhuis',
+   leiras: 'A Havenhuis 2016 óta az antwerpeni kikötői hivatal székhelye. Zaha Hadid brit-iraki sztárépítész tervezte, és építette egy 1922-ből származó műemlékvédelem alatt álló tűzoltósági épület tetejére.',
+ };
+ let kep4 = {
+   file: 'newyork-theshed.jpg',
+   cim: 'New York - The Shed',
+   leiras: 'A Shed egy kulturális központ a Manhattan-i Hudson Yards-ban. A 2019-ben megnyílt Shed az előadóművészet, a vizuális művészet és a popkultúra tevékenységeinek széles skáláját mutatja be.',
+ };
+ let kep5 = {
+   file: 'warszawa-spire.jpg',
+   cim: 'Varsó - Spire',
+   leiras: 'A Warsaw Spire a Varsóban található Neomodern irodaházak összessége, amelyet a belga ingatlanfejlesztő Ghelamco épített. Egy 220 méteres főtoronyból áll, hiperboloid üveghomlokzattal, valamint két 55 méteres kisegítő épületből.',
+ };
+ let kep6 = {
+   file: 'hochiminhcity-bitexcotower.jpg',
+   cim: 'Ho Si Minh-város - Bitexco Financial Tower',
+   leiras: 'A Bitexco Financial Tower egy felhőkarcoló Ho Si Minh -városban, Vietnamban.  Magassága 262,5 méter. A tornyot Carlos Zapata tervezte.',
+ };
+ let kep7 = {
+   file: 'kobenhavn-maersktower.jpg',
+   cim: 'Koppenhága - Maersk Tower',
+   leiras: 'A Maersk Tower a Koppenhágai Egyetem része. A torony eléri a 75 métert, a homlokzatot 3300 réz redőny borítja, ami a domborműszerű megjelenést kelti.',
+ };
+ let kep8 = {
+   file: 'london-thewalkietalkie.jpg',
+   cim: 'London - The Walkie-Talkie',
+   leiras: 'A 20 Fenchurch Street egy kereskedelmi felhőkarcoló Londoni City-ben. 160 m magas, alakja miatt a "The Walkie-Talkie" becenevet kapta. A 35. emeleten kilátó, bár és étterem található, ezek korlátozásokkal nyilvánosak.',
+ };
+ let kep9 = {
+  file: 'hamburg-elbphilharmonie.jpg',
+  cim: 'Hamburg - Elbphilharmonie',
+  leiras: 'Az Elbai Filharmónia Hamburg HafenCity nevű városrészében található koncerthelyszín. Egy alapítvány kezdeményezte a létrehozását, hogy a Sydney-i Operaházhoz hasonló jelképe legyen a városnak.',
+ }
+ let kep10 = {
+  file: 'kualalumpur.jpg',
+  cim: 'Kuala Lumpur - Petronas Twin Towers',
+  leiras: 'A Petronas-ikertorony az építtető kőolajtársaság (Petronas) nevét viselő 451,9 méter magas felhőkarcoló Kuala Lumpurban, Malajzia fővárosában.',
+ } 
+ let kep11 = {
+  file: 'moscow-internationalbusinesscenter.jpg',
+  cim: 'Moszkva - International Business Center',
+  leiras: 'A Moszkvai Nemzetközi Üzleti Központ (MIBC), egy épülő kereskedelmi fejlesztés Moszkvában. 60 hektár területet foglal el a Moszkva folyó partján, kb. 4 km -re nyugatra a Vörös tértől. A komplexum Európa egyik legnagyobb pénzügyi központja, itt található a legtöbb felhőkarcoló a kontinensen.',
+ }
+ let kep12 = {
+  file: 'newyork-56leonardstreet.jpg',
+  cim: 'New York - 56 Leonard Street',
+  leiras: 'A Leonard Street 56 (köznyelven Jenga-torony) egy 250 m magas, 57 emeletes felhőkarcoló Manhattanben. Az épületet a svájci Herzog & de Meuron építésziroda tervezte, amely az épületet az "égbe rakott házaknak" írja le.'
+ }
+ let kep13 = {
+  file: 'shenzhen-chinaresourcestower.jpg',
+  cim: 'Shenzhen - China Resources Tower',
+  leiras: 'A China Resources Tower, köznyelven tavaszi bambusz, egy 392 m magas szupertall felhőkarcoló.',
+ }
+ let kep14 = {
+  file: 'sidney-opera.jpg',
+  cim: 'Sidney - Opera',
+  leiras: 'A Sydney-i Operaház új épületének megépítésére 1954-ben pályázatot írtak ki,a nyertes Jørn Utzon dán építész lett. A terv ihletője Sydney hatalmas jachtkikötője, az ott gyakran látható vitorlák voltak.',
+ }
